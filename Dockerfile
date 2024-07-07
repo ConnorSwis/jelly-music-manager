051444b5212ac6c77186e748606553ad7b4fb5dc
ConnorSwis <condude13@gmail.com>
2024-07-07 10:29:28 -0400

    update `api.py`

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,4 @@ RUN spotdl --download-ffmpeg
 RUN dos2unix /app/entrypoint.sh
 RUN chmod +x /app/entrypoint.sh
 
-HEALTHCHECK --interval=30s --timeout=10s --start-period=5s CMD curl --fail http://localhost:8000/health || exit 1
-
 CMD ["/app/entrypoint.sh"]
--- app/routes/api.py
+++ app/routes/api.py
@@ -1,7 +1,6 @@
 import os
 import re
 import subprocess
-from multiprocessing import Pool
 from typing import List, Union
 from urllib.parse import urlparse, urlunparse
 
@@ -13,10 +12,10 @@ from app.models.spotify_types import Album, Artist, Playlist, Track
 
 api_router = APIRouter()
 
-tasks = []
+MUSIC_DIR = "music"
 
-if not os.path.exists("music"):
-    os.mkdir("music")
+if not os.path.exists(MUSIC_DIR):
+    os.mkdir(MUSIC_DIR)
 
 
 ############################## helper functions  ##############################
@@ -54,56 +53,61 @@ class AlbumDownload(BaseModel):
 
 
 ############################# download functions ##############################
-def _download_albums(album_urls: AlbumDownload):
-    with Pool(processes=2) as pool:
-        pool.map(_download_album, album_urls)
-
 def _create_m3u8(playlist):
     playlist, tracks = playlist
     m3u8_content = "#EXTM3U\n"
     m3u8_content += f"#SPOTIFY-ID:{playlist['id']}\n"
     m3u8_content += "\n".join([f"{_clean_filename(track['album_name'])}/{_clean_filename(track['name'])}.mp3" for track in tracks])
-    m3u8_path = os.path.join("./music", f"{playlist['name']}.m3u8")
+    m3u8_path = os.path.join(MUSIC_DIR, f"{playlist['name']}.m3u8")
 
     with open(m3u8_path, "w") as m3u8_file:
         m3u8_file.write(m3u8_content)
 
     return playlist['name'], m3u8_path
 
+
 def _download_album(url_or_album: Union[str, Album]):
+    url, album = None, None
+
     if isinstance(url_or_album, str):
         url = url_or_album
         album = Album.get_metadata(url)
     else:
         album = url_or_album
         url = album[0]['url']
+
     try:
         name = _clean_filename(album[0]['name'])
-        base_dir = "./music"
-        album_dir = os.path.join(base_dir, name)
+        album_dir = os.path.join(MUSIC_DIR, name)
         os.makedirs(album_dir, exist_ok=True)
         spotdl_file = re.sub(r'[^\w\-_\.]', '', name.replace(' ', '')) + '.spotdl'
-        command = f"spotdl sync {url} --save-file {spotdl_file}"
-        process = subprocess.Popen(command.split(), cwd=album_dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        command = ["spotdl", "sync", url, "--save-file", spotdl_file]
+        process = subprocess.Popen(command, cwd=album_dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = process.communicate()
-        if process.returncode != 0:
-            raise HTTPException(status_code=500, detail=stderr)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Failed to download {name} from {url}: {str(e)}")
 
-def _download(data):
+        if process.returncode != 0:
+            error_message = stderr.decode('utf-8')
+            raise HTTPException(status_code=500, detail=f"Download failed for album '{name}' from '{url}': {error_message}")
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to download album '{name}' from '{url}': {str(e)}")
+
+
+def _download(data, background_tasks: BackgroundTasks):
     resource = data["resource"]
     data = data["data"]
-    match resource:
-        case "album":
-            _download_album(data)
-        case "artist":
-            _download_albums(data[0]['albums'])
-        case "playlist":
-            _create_m3u8(data)
-            _download_albums(data[0]['albums'])
-        case "track":
-            _download_album(data["album_url"])
+    if resource == "album":
+        background_tasks.add_task(_download_album, data)
+    elif resource == "artist":
+        for album in data[0]['albums']:
+            background_tasks.add_task(_download_album, album)
+    elif resource == "playlist":
+        _create_m3u8(data)
+        for album in data[0]['albums']:
+            background_tasks.add_task(_download_album, album)
+    elif resource == "track":
+        background_tasks.add_task(_download_album, data["album_url"])
+    return data
 
 
 ################################# controller #################################
@@ -126,9 +130,9 @@ class SpotifyController:
         return {"resource": resource, "data": data}
     
     @classmethod
-    def download(cls, spotify_url: SpotifyURL):
+    def download(cls, spotify_url: SpotifyURL, background_tasks: BackgroundTasks):
         query = cls.query(spotify_url)
-        _download(query)
+        _download(query, background_tasks)
         return query
 
 
@@ -138,6 +142,5 @@ async def query(spotify_url: SpotifyURL = Depends()):
     return SpotifyController.query(spotify_url)
 
 @api_router.get("/download/", summary="Download content based on Spotify URL")
-async def download(spotify_url: SpotifyURL = Depends()):
-    BackgroundTasks().add_task(SpotifyController.download(spotify_url))
-    return 
+async def download(background_tasks: BackgroundTasks, spotify_url: SpotifyURL = Depends()):
+    return SpotifyController.download(spotify_url, background_tasks)
