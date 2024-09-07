c2a70675e29643cd94ef0897817b38d4b61eb019
Connor Swislow <52296992+ConnorSwis@users.noreply.github.com>
2024-09-07 14:45:21 -0400

    Update spotify_types.py

--- app/models/spotify_types.py
+++ app/models/spotify_types.py
@@ -8,45 +8,27 @@ from spotdl.types.song import Song
 
 __all__ = ["Album", "Artist", "Playlist", "Track"]
 
-_client_id = _os.getenv("SPOTIFY_ID")
-_client_secret = _os.getenv("SPOTIFY_SECRET")
-_spotify = _SpotifyClient.init(client_id=_client_id, client_secret=_client_secret)
 
-_d=["album_name", "name", "year", "artists", "duration", "song_id", "album_id",]
-def _filter_song_attributes(songs, attributes=_d):
-    if len(attributes) == 0:
-        return list(songs)
-    filtered_songs = [
-        {attr: obj.__dict__[attr] for attr in attributes if attr in obj.__dict__}
-        for obj in songs
-    ]
-    return filtered_songs
+spotify = SpotifyClient()
 
-def _duration(songs: _List[_Dict[str, _Any]]) -> str:
-    total_duration = sum(song.duration for song in songs)
-    return total_duration
 
-class Album(_Album):
+class Album(Album_):
     @staticmethod
-    def get_metadata(url: str, attributes=_d) -> _Tuple[_Dict[str, _Any], _List[_Song]]:
-        metadata, songs = _Album.get_metadata(url)
-        metadata.update(_spotify.album(url))
-        metadata["total_duration"] = _duration(songs)
-        metadata["tracks"] = len(songs)
-        return metadata, _filter_song_attributes(songs, attributes=attributes)
+    def get_metadata(url: str) -> Tuple[Dict[str, Any], List[Song]]:
+        metadata, songs = Album_.get_metadata(url)
+        metadata.update(spotify.album(url))
+        return metadata, songs
 
 
-class Artist(_Artist):
+class Artist(Artist_):
     @staticmethod
-    def get_metadata(url: str, attributes=_d) -> _Tuple[_Dict[str, _Any], _List[_Song]]:
-        metadata, songs = _Artist.get_metadata(url)
-        metadata.update(_spotify.artist(url))
-        metadata["total_duration"] = _duration(songs)
-        metadata["tracks"] = len(songs)
-        return metadata, _filter_song_attributes(songs, attributes=attributes)
+    def get_metadata(url: str) -> Tuple[Dict[str, Any], List[Song]]:
+        metadata, songs = Artist_.get_metadata(url)
+        metadata.update(spotify.artist(url))
+        return metadata, songs
 
 
-class Playlist(_Playlist):
+class Playlist(Playlist_):
     @staticmethod
     def get_metadata(url: str):
         metadata, songs = Playlist_.get_metadata(url)
@@ -55,7 +37,7 @@ class Playlist(_Playlist):
         return metadata, songs, unique_song_ids
 
 
-class Track(_Song):
+class Track(Song):
     @staticmethod
     def get_metadata(url): 
         song = Song.from_url(url)
