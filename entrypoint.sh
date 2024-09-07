8ca00851e101d15b3615b0134cf5933fcf476dd1
Connor Swislow <52296992+ConnorSwis@users.noreply.github.com>
2024-09-07 14:54:43 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Start the FastAPI application
-python -m uvicorn main:app --host 0.0.0.0 --port 8000 &
+python -m uvicorn main:app --host 0.0.0.0 --port 80 &
 
 # Capture the PID of the FastAPI process
 PID=$!
